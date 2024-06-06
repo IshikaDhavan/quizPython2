a147a03de0fef93d8a1dd7293102414f0c1dcf2a
IshikaDhavan <107250389+IshikaDhavan@users.noreply.github.com>
2024-06-06 15:28:13 +0530

    Add files via upload

---
+++ quiz_client.py
@@ -0,0 +1,35 @@
+import socket
+from threading import Thread
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+name = "Write your name: "
+ipaddr = "127.0.0.1"
+port = 7500
+
+client.connect((ipaddr,port))
+print("connected with server")
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if message == 'NICKNAME':
+                client.send(name.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("An error occured")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = '{} : {}'.format(name,input(''))
+        client.send(message.encode('utf-8'))
+
+recieve_thread = Thread(target= recieve)
+recieve_thread.start()
+
+write_thread = Thread(target= write)
+write_thread.start()
